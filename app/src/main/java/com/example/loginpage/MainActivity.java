5654f8fecc091b72fa22bd7e36d811fefc9e8fbe
ummeu <ummeummehafsa55@gmail.com>
2025-07-18 14:06:44 +0600

    itial the commit message

---
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -0,0 +1,33 @@
+package np.com.bimalkafle.loginpage;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText usernameInput; // Java-style declaration
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Link the EditText with the one in the layout
+        usernameInput = findViewById(R.id.usernameInput);
+
+        // Optional: Now you can use usernameInput.getText().toString();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
